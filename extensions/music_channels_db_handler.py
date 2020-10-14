2849158fbf8934f189f0b0a13eb67d83b094f867
E. Fine <immortalisEL@gmail.com>
2020-10-15 01:56:48 +0300

    Added music_channels_db handling for leaving server event

--- extensions/music_channels_db_handler.py
+++ extensions/music_channels_db_handler.py
@@ -3,6 +3,12 @@ from discord.ext import commands
 from utils.utils import log_event, MUSIC_CHANNELS_PATH
 
 
+def remove_server(gid):
+    music_channels = json.load(open(MUSIC_CHANNELS_PATH, 'r'))
+    music_channels.pop(str(gid))
+    json.dump(music_channels, open(MUSIC_CHANNELS_PATH, 'w'), indent=4)
+
+
 class MusicChannelsDBHandler(commands.Cog):
     def __init__(self, _bot):
         self.bot = _bot
@@ -11,21 +17,27 @@ class MusicChannelsDBHandler(commands.Cog):
     async def on_ready(self):
         log_event(f'{self.qualified_name} extension loaded')
 
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        remove_server(guild.id)
+
     @commands.command(brief="Set a text channel to a music spam channel")
     @commands.has_permissions(administrator=True)
     async def setmusic(self, ctx):
         music_channels = json.load(open(MUSIC_CHANNELS_PATH, 'r'))
         music_channels[str(ctx.guild.id)] = ctx.channel.id
         json.dump(music_channels, open(MUSIC_CHANNELS_PATH, 'w'), indent=4)
-        await ctx.send(f'{ctx.author.mention} {ctx.channel.name} is now set as the music spam channel for this server')
+        message = f"{ctx.channel.name} is now set as the music spam channel for the server '{ctx.guild}'"
+        log_event(message)
+        await ctx.send(f'{ctx.author.mention} {message}')
 
     @commands.command(brief="Delete the music spam setting for a server")
     @commands.has_permissions(administrator=True)
     async def delmusic(self, ctx):
-        music_channels = json.load(open(MUSIC_CHANNELS_PATH, 'r'))
-        music_channels.pop(str(ctx.guild.id))
-        json.dump(music_channels, open(MUSIC_CHANNELS_PATH, 'w'), indent=4)
-        await ctx.send(f'{ctx.author.mention} music spam settings for this server have been deleted')
+        remove_server(ctx.guild.id)
+        message = f"music spam settings for the server '{ctx.guild}' have been deleted"
+        log_event(message)
+        await ctx.send(f'{ctx.author.mention} {message}')
 
 
 # expected function for outside calling function 'load_extension()'
